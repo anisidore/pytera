783578f5a47b93394afead0957110b39bf9daa86
Kere <keresifon.isidore@gmail.com>
2022-02-13 23:37:11 -0500

    update to cluster code

--- kubernetes/eks-cluster.tf
+++ kubernetes/eks-cluster.tf
@@ -1,29 +1,68 @@
+// module "eks" {
+//   source          = "terraform-aws-modules/eks/aws"
+//   cluster_name    = var.cluster_name
+//   cluster_version = "1.20"
+//   subnet_ids   = module.vpc.private_subnets
+
+//   tags = {
+//     Environment = "training"
+//     GithubRepo  = "EKS-TERRAFORM"
+//     GithubOrg   = "terraform-aws-modules"
+//   }
+
+//   vpc_id = module.vpc.vpc_id
+
+  
+//     eks_managed_node_group_defaults = {
+//     root_volume_type = "gp2"
+//   }
+
+//   eks_managed_node_groups = [
+//     {
+//       name                          = "worker-group-1"
+//       instance_type                 = "t2.small"
+//       additional_userdata           = "echo foo bar"
+//       asg_desired_capacity          = 2
+//       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
+//     },
+//     {
+//       name                          = "worker-group-2"
+//       instance_type                 = "t2.small"
+//       additional_userdata           = "echo foo bar"
+//       additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
+//       asg_desired_capacity          = 1
+//     },
+//   ]
+// }
+
+// data "aws_eks_cluster" "cluster" {
+//   name = module.eks.cluster_id
+// }
+
+// data "aws_eks_cluster_auth" "cluster" {
+//   name = module.eks.cluster_id
+// }
+//Old Code Update below
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
+  version         = "17.24.0"
   cluster_name    = var.cluster_name
   cluster_version = "1.20"
-  subnet_ids   = module.vpc.private_subnets
-
-  tags = {
-    Environment = "training"
-    GithubRepo  = "EKS-TERRAFORM"
-    GithubOrg   = "terraform-aws-modules"
-  }
+  subnets         = module.vpc.private_subnets
 
   vpc_id = module.vpc.vpc_id
 
-  
-    eks_managed_node_group_defaults = {
+  workers_group_defaults = {
     root_volume_type = "gp2"
   }
 
-  eks_managed_node_groups = [
+  worker_groups = [
     {
       name                          = "worker-group-1"
       instance_type                 = "t2.small"
       additional_userdata           = "echo foo bar"
-      asg_desired_capacity          = 2
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
+      asg_desired_capacity          = 2
     },
     {
       name                          = "worker-group-2"
